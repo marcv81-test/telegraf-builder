dcb3a3b266acfb8fb7f863dff9e6adc921dc5b95
Marc <marc.venturini@gmail.com>
2019-04-24 23:44:52 +0800

    More flexible environment, Telegraf 1.10.3 ready

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ This is a Docker container to build Telegraf.
 
 ## Setup
 
-    ./build.sh
     ./start.sh
     git clone https://github.com/influxdata/telegraf.git src/github.com/influxdata/telegraf
     cd src/github.com/influxdata/telegraf
--- start.sh
+++ start.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+if [ -z "$HOME" ]
+then
+  echo "HOME not defined"
+  exit 1
+fi
+
+USER_ID=`id -u`:`id -g`
+
+echo "HOME=$HOME"
+echo "USER_ID=$USER_ID"
+
+docker build docker \
+  --build-arg HOME="$HOME" \
+  --build-arg USER_ID="$USER_ID" \
+  -t golang-builder
+
 docker run \
-  -it --rm -u=`id -u`:`id -g` \
-  -v `pwd`/go:/home/go \
+  -it --rm -u="$USER_ID" \
+  -v "`pwd`/go":"$HOME/go" \
   -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group \
+  -v /var/run/utmp:/var/run/utmp \
   golang-builder
